8dbbebf94094b83b867c299d9a22e2722a7feb8a
Martyna Lepa <martyna.lepa@student.pk.edu.pl>
2026-06-14 15:52:55 +0200

    Fix: naprawa

--- backend/src/main/java/pk/lm/pasir_lepa_martyna/model/Transaction.java
+++ backend/src/main/java/pk/lm/pasir_lepa_martyna/model/Transaction.java
@@ -1,5 +1,4 @@
 package pk.lm.pasir_lepa_martyna.model;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,11 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-
-/**
- * The Transaction entity represents a single financial transaction.
- * Each transaction has a unique identifier, amount, type, tags, notes, and creation date.
- */
 
 @Entity
 @Getter
@@ -24,21 +18,14 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Double amount;
-
-    @Compass(EnumType.STRING)
     @Enumerated(EnumType.STRING)
     private TransactionType type;
-
     private String tags;
-
     private String notes;
-
     private LocalDateTime timestamp;
 
     public Transaction(Double amount, TransactionType type, String tags, String notes, User user) {
@@ -47,7 +34,6 @@ public class Transaction {
         this.tags = tags;
         this.notes = notes;
         this.user = user;
-        // POPRAWKA: Dodaliśmy jawne wskazanie strefy czasowej (systemowej), co uciszy błąd w Sonarze
         this.timestamp = LocalDateTime.now(ZoneId.systemDefault());
     }
 }
