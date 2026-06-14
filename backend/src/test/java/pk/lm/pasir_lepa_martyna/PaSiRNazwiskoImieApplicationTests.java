42af86d13d2ff447109cb71bd94aebace1b2db35
Martyna Lepa <martyna.lepa@student.pk.edu.pl>
2026-06-14 14:45:59 +0200

    Fix: Podmiana konfiguracji w glownym pliku testowym

--- backend/src/test/java/pk/lm/pasir_lepa_martyna/PaSiRNazwiskoImieApplicationTests.java
+++ backend/src/test/java/pk/lm/pasir_lepa_martyna/PaSiRNazwiskoImieApplicationTests.java
@@ -1,9 +1,9 @@
-package pk.ni.pasir_nazwisko_imie;
+package pk.lm.pasir_lepa_martyna;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PasirLepaMartynaApplication.class)
 class PaSiRNazwiskoImieApplicationTests {
 
     @Test
